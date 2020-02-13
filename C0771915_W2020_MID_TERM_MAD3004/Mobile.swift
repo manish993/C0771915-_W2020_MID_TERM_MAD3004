3a84001e7b4466e6edf7bab18346cf040db83daf
Manish <manishpawar94638@gmail.com>
2020-02-13 13:44:55 -0500

    [mp] adding members to mobile class

--- C0771915_W2020_MID_TERM_MAD3004/Mobile.swift
+++ C0771915_W2020_MID_TERM_MAD3004/Mobile.swift
@@ -5,5 +5,17 @@
 //  Created by MacStudent on 2020-02-13.
 //  Copyright © 2020 MacStudent. All rights reserved.
 //
+enum Plan
+{
+    case weekly,monthly,Annual
+}
 
 import Foundation
+class Mobile:Bill
+{
+    var manufacturerName:String
+    var plan:Plan
+    var mobileNumber:Int
+    var gbUsed:Int
+    var minutesUsed:Int
+}
