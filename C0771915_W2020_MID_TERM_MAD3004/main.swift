3acfbc7ac135c9ca0d8c158268524f3d08d46edc
Manish <manishpawar94638@gmail.com>
2020-02-13 13:16:06 -0500

    [mp]creation of git

---
+++ C0771915_W2020_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  C0771915_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+print("project")
+
